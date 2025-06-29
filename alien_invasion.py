5b32f1eda751c8bf80237a6cb48ffb7531b5e95d
Anik Chaudhary <aa4639@srmist.edu.in>
2025-06-29 20:14:23 +0530

    Moving the Aliens Right

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,8 +36,8 @@ class AlienInvasion:
     while True:
       self._check_events()
       self.ship.update()
-      
       self._update_bullets()
+      self._update_aliens()
       self._update_screen()
       self.clock.tick(60)
       
@@ -47,11 +47,8 @@ class AlienInvasion:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         sys.exit()
-
       elif event.type == pygame.KEYDOWN:
         self._check_keydown_events(event)
-        
-
       elif event.type == pygame.KEYUP:
         self._check_keyup_events(event)
         
@@ -89,24 +86,34 @@ class AlienInvasion:
     for bullet in self.bullets.copy():
       if bullet.rect.bottom <= 0:
         self.bullets.remove(bullet)
+        
+  def _update_aliens(self):
+    """Update the positions of all aliens in the fleet."""
+    self.aliens.update()
     
   def _create_fleet(self):
     """Create the fleet of aliens."""
     # Create an alien and keep adding aliens until there's no room left.
-    # Spacing between aliens is one alien width.
+    # Spacing between aliens is one alien width and one alien height.
     alien = Alien(self)
-    alien_width = alien.rect.width
+    alien_width, alien_height = alien.rect.size
     
-    current_x = alien_width
-    while current_x < (self.settings.screen_width - 2 * alien_width):
-      self._create_alien(current_x)
-      current_x += 2 * alien_width
+    current_x, current_y = alien_width, alien_height
+    while current_y < (self.settings.screen_height - 3 * alien_height):
+      while current_x < (self.settings.screen_width - 2 * alien_width):
+        self._create_alien(current_x, current_y)
+        current_x += 2 * alien_width
       
-  def _create_alien(self, x_position):
-    """Create an alien and place it in the row."""
+      # Finished a row; reset x value, and increment y value.
+      current_x = alien_width
+      current_y += 2 * alien_height
+      
+  def _create_alien(self, x_position, y_position):
+    """Create an alien and place it in the fleet"""
     new_alien = Alien(self)
     new_alien.x = x_position
     new_alien.rect.x = x_position
+    new_alien.rect.y = y_position
     self.aliens.add(new_alien)
 
 
