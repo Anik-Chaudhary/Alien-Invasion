d734bc51f43a0181389e85900b1818122739e7a4
Anik Chaudhary <aa4639@srmist.edu.in>
2025-07-23 22:54:41 +0530

    Changes to Scoreboard.py and Updating the Score as Aliens Are Shot Down

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -25,8 +26,10 @@ class AlienInvasion:
     self.settings.screen_height = self.screen.get_rect().height
     pygame.display.set_caption("Alien Invasion")
     
-    # Create an instance to store game statistics.
+    # Create an instance to store game statistics,
+    # and create a scoeboard.
     self.stats = GameStats(self)
+    self.sb = Scoreboard(self)
 
     self.ship = Ship(self)
     self.bullets = pygame.sprite.Group()
@@ -77,7 +80,7 @@ class AlienInvasion:
     button_clicked = self.play_button.rect.collidepoint(mouse_pos)
     if button_clicked and not self.game_active:
       # Reset the game settings.
-      self.settings.initalize_dynamic_settings()
+      self.settings.initialize_dynamic_settings()
       
       # Reset the game statistics.
       self.stats.reset_stats()
@@ -137,6 +140,10 @@ class AlienInvasion:
     collisions = pygame.sprite.groupcollide(
       self.bullets, self.aliens, True, True
     )
+    
+    if collisions:
+      self.stats.score += self.settings.alien_points 
+      self.sb.prep_score()
     
     if not self.aliens:
       # Destroy existing bullets and create new fleet.
@@ -204,6 +211,9 @@ class AlienInvasion:
       bullet.draw_bullet()
     self.ship.blitme()
     self.aliens.draw(self.screen)
+    
+    # Draw the score information.
+    self.sb.show_score()
     
     # Draw the play button if the game is inactive.
     if not self.game_active:
--- scoreboard.py
+++ scoreboard.py
@@ -22,7 +22,7 @@ class Scoreboard:
     self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
     
     # Display the score at the top right of the screen.
-    self.screen_rect = self.score_image.get_rect()
+    self.score_rect = self.score_image.get_rect()
     self.score_rect.right = self.screen_rect.right - 20
     self.score_rect.top = 20
     
