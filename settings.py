4f5ebad2c287e930730b41d2dd535aca91613fa5
Anik Chaudhary <aa4639@srmist.edu.in>
2025-07-23 23:13:15 +0530

    Changing bullet width, Increasing Point Values

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings:
     
     # Bullet settings
     self.bullet_speed = 2.5
-    self.bullet_width = 300
+    self.bullet_width = 3
     self.bullet_height = 15
     self.bullet_color = (60, 60, 60)
     self.bullets_allowed = 3 
@@ -24,6 +24,9 @@ class Settings:
     
     # How quickly the game speeds up
     self.speedup_scale = 1.1
+    
+    # How quickly the alien point values increase
+    self.score_scale = 1.5
     
     self.initialize_dynamic_settings()
     
@@ -40,8 +43,11 @@ class Settings:
     self.alien_points = 50
     
   def increase_speed(self):
-    """Increase speed settings."""
+    """Increase speed settings and alien point values."""
     self.ship_speed *= self.speedup_scale
     self.bullet_speed *= self.speedup_scale
     self.alien_speed *= self.speedup_scale
+    
+    self.alien_points = int(self.alien_points * self.score_scale)
+    print(self.alien_points)
     
